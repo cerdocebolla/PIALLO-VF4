183cd7b0eb90e433b5689cd618a38ec0b66dc322
cerdocebolla software <lordofsuperium17@gmail.com>
2025-05-25 08:23:33 +0000

    First files added to PIALLO VF4

---
+++ pialloVF4/wifiFinder.lua
@@ -0,0 +1,41 @@
+local function = true 
+    when askedForWifI = true 
+    then let function = goDown then 
+        let print ("Searching the best Wi-Fi connetion for your device. Please stand by.")
+        then let function = endInstant.end 
+    end)
+}
+
+when function = true 
+    then createChain = CreateFunction then goDown 
+        askedForFunction = goDown 
+        then 
+    createChain = goDown 
+    then readDown checkDown
+        localClient.serverClient.LocalUsers.mainUser.user.findWiFi = then 
+            must 
+            force 
+        for function = setTrue = true 
+            when askedForFunction = WiFiFinding.function = true 
+                then let function goDown
+                endInstant.end 
+                end)
+            end)
+        end)
+    end)
+}
+
+when function = chain = functioned 
+    as true 
+    then 
+        
+        for function = try.ConnectWiFi = mustDo
+            when = true 
+            then let print = goDown
+                function = let print ("PIALLO has found the best Wi-Fi connection for your device.")
+                    then endInstant.end 
+                end) 
+            end)
+        end)
+    end)
+}
---
+++ pialloVF4/wifiFinderHandler.rs
@@ -0,0 +1,18 @@
+local function = true 
+when setTrue = findingWiFi = goDown
+must.force = true 
+then function = print 
+let print ("The PIALLO Software has found the best Wi-Fi connection for your device. Please wait for PIALLO to set itself up.")
+    then askForFunction = Location.FindingWiFi = must.force = true 
+    for function = findingWiFi GoDown (ReadDown.Read)
+    then for FunctioningWiFi = must.Force = AskForSignal then 
+    CreateParent.new = true 
+            set Parent.SideEffect.Name = signal GoDown 
+            GoUp (ReadUp) = true 
+            must.Force 
+        to FindSignal = depend 
+    on location = activatePIALLO = pinning 
+    when LocalChild = pinning = true 
+    then let function endInstant.end 
+    end)
+}
