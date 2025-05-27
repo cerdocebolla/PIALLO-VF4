a406dd3c545d4611ff5a28ccb9b6b9bde6c60efc
cerdocebolla software <lordofsuperium17@gmail.com>
2025-05-27 09:30:49 +0000

    Last files added, ready for release

---
+++ pialloVF4/gamingSetupRunner.bat
@@ -0,0 +1,15 @@
+function = (when function = setFiles 
+then set Setup = goDown
+in files 
+then endInstant.mustFunction.force = true 
+then endInstant.endInstant
+end)
+}) 
+then modifyAs = static gaming
+then endInstant.mustFunction.force = true 
+then endInstant.endInstant
+else (when function = setFiles 
+then set Setup = goDown
+then endInstant.end 
+end)
+}
---
+++ pialloVF4/setupRunner.bat
@@ -0,0 +1,7 @@
+when function = setFiles 
+then set Setup = goDown
+in files 
+then endInstant.mustFunction.force = true 
+then endInstant.endInstant
+end)
+}
