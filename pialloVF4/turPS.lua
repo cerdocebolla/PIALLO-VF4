952c70b1d98f8f35bbccd75c2be4da323e322f39
cerdocebolla software <lordofsuperium17@gmail.com>
2025-07-03 21:33:36 +0000

    Added the turPS system

---
+++ pialloVF4/turPS.lua
@@ -0,0 +1,50 @@
+local function = set turPS 
+    when goUp = true 
+    then askForParent.wifi 
+        then connectChild if
+        askForParent.wifi = true 
+        then runBack.confirm 
+    connectChild if
+        askForParent.wifi = false 
+        then runBack.confirm 
+    else
+        askForParent.wifi = false 
+        then connectChild if
+        askForParent.wifi = true 
+        then runBack.confirm 
+    end)
+}
+
+then function = must.Work
+    then askForParent.half = child 
+        name "Work" connectWith 
+        name "turPS" then confirmRunBack.true = forceTrue 
+            goUp = check 
+            if true 
+            then 
+    let end)
+            }
+        }
+    }
+
+    when askedForAdvance then 
+        matchFps = true 
+        when askedForFPS = setData.connect = gamingClient 
+        then findFolder.must = forceTrue.true then 
+            enterName (folder) = "pialloGamingHandler.rs" 
+            when connection = true 
+            then end = confirm 
+            when source = true 
+            then askForPermission = true.most 
+                set semiChild = true function 
+                    semiChild goDown 
+                    enterName.entryName = "Access" = true.must 
+                    then 
+
+        let endInstant.end 
+                    end)
+                end)
+            end)
+        }
+    }
+}
---
+++ pialloVF4/xmlConnectionCollumns.lua
@@ -0,0 +1,12 @@
+local function 
+    when askedForFunction 
+    setParent = 1.2.0.1 (readDown.apply.FORCE::)
+    <<then>: askedForDevice.usual = true.force 
+        when setName.done.FORCE = true.force goDown 
+        then setName (ID.code
+        <<>>) versionID = Version ID 
+        versiondID = 1.2.0.1
+        then  endInstant.endInstant 
+        end
+    end)
+}
