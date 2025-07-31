f8500bd42c890a237aada7b6e20b78abb7943a8b
cerdocebolla software <lordofsuperium17@gmail.com>
2025-07-31 19:02:12 +0000

    Added the turPS system.

---
+++ pialloVF4/cerdoAdvance.java
@@ -0,0 +1,32 @@
+instant static 
+code askedForSlash.JAVASLASH = inputOut.Guardar
+let 
+else if askedForSlash=setAs 
+no space 
+mainAction.print.logg = advance.fastApply DOWN
+        SWAPaccess.IOS.Android=advance 
+                else if 
+        SWAPaccess.PC.Console=advance 
+    start.Cycle = staticPublic 
+    when TIMEOUT 
+    DOWN SetPrivate.access = able
+    transfer    
+    passedTIMEFORMAT.true = forcedStatic APPLY DOWN
+    =setTrue private STATIC servers.client 
+    created CHAIN            
+                                        ACCESSdenied
+                        ACCESSapplied.forced TO true 
+        else if NOTaccessible=true 
+        then softwareINPUT.log.=guardar 
+        Guardar NAMEDSOFTWAREsoftware = true 
+then makeAble.INSTANCE.new 
+    readCODE.logADVANCE 
+    when ADVANCED = static 
+    PUBLIC SERVER = full 
+    serverFULL = DISABLE gUARDAR 
+        WAIT (0) lowercaseINPUT.log.out.=Guardar
+
+        then endInstant.end 
+        end)
+    }
+}
---
+++ pialloVF4/main_turPS.lua
@@ -0,0 +1,58 @@
+local studioAccess.turPS 
+down = activated when askedFor 
+staticInstance.true = then 
+    if not 
+    askedFor software = staticNetwork
+    connect gamingParent
+    actiave.STRICTLAW.tabs = true.force 
+                then askkedForInstance = true.force 
+                then setName (ID.code
+                <<>>) versionID = Version ID
+                versiondID = 01943781490283945
+                then endInstant.endInstant 
+                end
+            else if askedFor=endInstance 
+    newStaticInstance = true.force 
+    then setName (ID.code0000 = true.force 
+        when takenData#xmlCollumns = true 
+                            then enterDataNames = FPS  
+                            else if NUMBERSSET.true = force 
+                    versionID.connect = CercenseSubscriptions.PAIDACCESS
+        when mainUser=paidACCESS.force = true 
+then let endInstant.endInstant 
+        end)
+    end)
+end
+
+when askedFor = true.force 
+    then setName (ID.code0000 = true.force 
+        when takenData#xmlCollumns = true 
+                            then enterDataNames = FPS  
+                            else if NUMBERSSET.true = force 
+                    versionID.connect = CercenseSubscriptions.PAIDACCESS
+        when mainUser=paidACCESS.force = true 
+then let endInstant.endInstant 
+        end)
+end
+when askedFor = true.force 
+    then setName (ID.code0000 = true.force 
+        when takenData#xmlCollumns = true 
+                            then enterDataNames = FPS  
+                            else if NUMBERSSET.true = force 
+                    versionID.connect = CercenseSubscriptions.PAIDACCESS
+        when mainUser=paidACCESS.force = true 
+then let endInstant.endInstant 
+        end)
+end
+
+when askedFor = true.force 
+    then setName (ID.code0000 = true.force 
+        when takenData#xmlCollumns = true 
+                            then enterDataNames = FPS  
+                            else if NUMBERSSET.true = force 
+                    versionID.connect = CercenseSubscriptions.PAIDACCESS
+        when mainUser=paidACCESS.force = true 
+then let endInstant.endInstant 
+        end)
+end 
+}
